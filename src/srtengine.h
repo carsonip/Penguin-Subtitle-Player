aaa4773c8812b856cdd036a8085205c42da92fac
LMH <pc20071139@gmail.com>
2015-02-28 14:59:14 +0800

    Fix coding style

--- src/srtengine.h
+++ src/srtengine.h
@@ -8,48 +8,70 @@
 class SrtEngine
 {
 
-    public:
+public:
 
-        SrtEngine(QString path, QString encoding);
-        ~SrtEngine();
-        struct SubtitleItem;
+    SrtEngine(QString path, QString encoding);
+    ~SrtEngine();
 
-    protected:
-        // Specifying new line character and tabs in regex
-        const QString nl = "\\n";
-        const QString sp = "[ \\t]*";
-    private:
-        std::vector<SubtitleItem> subtitles;
-        int lastIndex = -1;
-        long long finishTime = 0LL;
-    private:
-        static long long calculateTime(QString h, QString m, QString s, QString ms){
-            // Converting time information to milliseconds
-            bool res;
-            return h.toLongLong(&res, 10) * 60 * 60 * 1000 + m.toLongLong(&res, 10) * 60 * 1000 + s.toLongLong(&res, 10) * 1000 + ms.toLongLong(&res, 10);
+public:
+
+    struct SubtitleItem{
+        /* Structure for storing each subtitle,
+             * start, end stands for the starting and ending time in milliseconds,
+             * text stands for the content.
+            */
+
+        long long start, end;
+        QString text;
+        SubtitleItem(long long start, long long end, QString text)
+        {
+            this->start = start;
+            this->end = end;
+            this->text = text;
         }
-        static QString formatTime(long long x){
-            if(x < 10) return "0" + QString::number(x, 10);
-            else return QString::number(x, 10);
-        }
-    public:
-        static QString millisToTimeString(long long ms){
-            // Converting milliseconds to string in HH:MM:SS format
+    };
 
-            int hours = ms / 60 / 60 / 1000;
-            ms %= 60 * 60 * 1000;
-            int minutes = ms / 60 / 1000;
-            ms %= 60 * 1000;
-            int seconds = ms / 1000;
-            return formatTime(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds);
+    static QString millisToTimeString(long long ms)
+    {
+        // Converting milliseconds to string in HH:MM:SS format
 
-        }
-    public:
-        // Fetch the suitable subtitle content for current time
-        QString currentSubtitle(long long time, bool sliderMoved);
+        int hours = ms / 60 / 60 / 1000;
+        ms %= 60 * 60 * 1000;
+        int minutes = ms / 60 / 1000;
+        ms %= 60 * 1000;
+        int seconds = ms / 1000;
+        return formatTime(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds);
 
-        // Fetch the end time of last subtitle
-        long long getFinishTime();
+    }
+
+    static long long calculateTime(QString h, QString m, QString s, QString ms)
+    {
+        // Converting time information to milliseconds
+        bool res;
+        return h.toLongLong(&res, 10) * 60 * 60 * 1000 + m.toLongLong(&res, 10) * 60 * 1000 + s.toLongLong(&res, 10) * 1000 + ms.toLongLong(&res, 10);
+    }
+
+    static QString formatTime(long long x)
+    {
+        if(x < 10) return "0" + QString::number(x, 10);
+        else return QString::number(x, 10);
+    }
+
+    // Fetch the suitable subtitle content for current time
+    QString currentSubtitle(long long time, bool sliderMoved);
+
+    // Fetch the end time of last subtitle
+    long long getFinishTime();
+
+private:
+
+    // Specifying new line character and tabs in regex
+    const QString nl = "\\n";
+    const QString sp = "[ \\t]*";
+
+    std::vector<SubtitleItem> subtitles;
+    int lastIndex = -1;
+    long long finishTime = 0LL;
 
 };
 
