2b0814c30354d79f134bb21a6e5ac6eff917f147
Carson Ip <carsonip715@gmail.com>
2017-02-12 16:16:40 +0100

    Change org name

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     a.setApplicationDisplayName("Penguin Subtitle Player");
-    QCoreApplication::setOrganizationName("Carsons Lab");
-    QCoreApplication::setOrganizationDomain("carsonslab.com");
+    QCoreApplication::setOrganizationName("Carson Ip");
+    QCoreApplication::setOrganizationDomain("carsonip.github.io");
     QCoreApplication::setApplicationName("Penguin Subtitle Player");
     MainWindow w;
     w.show();
