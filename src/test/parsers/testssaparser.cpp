287255fcfe05173b6e985a0a589945213b460db6
Carson Ip <carsonip715@gmail.com>
2017-02-24 15:10:41 +0100

    Update SSA test

--- src/test/parsers/testssaparser.cpp
+++ src/test/parsers/testssaparser.cpp
@@ -5,8 +5,8 @@
 TestSsaParser::TestSsaParser() {}
 
 void TestSsaParser::testFormatText() {
-    QString inputText = "{\\i1}Italics{\\i0}\\n{\\b1}Bold{\\b0}\\N{\\u1}"
-                        "Underline{\\u0}{\\s1}Strikeout{\\s0}{\\ignoreme}";
+    QString inputText =
+        R"({\i1}Italics{\i0}\n{\b1}Bold{\b0}\N{\u1}Underline{\u0}{\s1}Strikeout{\s0}{\ignoreme})";
     QString expected = "<i>Italics</i><br><b>Bold</b><br><u>Underline</"
                        "u><s>Strikeout</s>";
     QString actual = SsaParser::formatText(inputText);
