eb7186c20adc0c8ed43eebd5342f5b4fda0df26c
Carson Ip <carsonip715@gmail.com>
2020-06-22 23:43:09 +0800

    Fix non-ASCII path IO error in Windows
    Fix #72

--- src/chardet.cpp
+++ src/chardet.cpp
@@ -1,22 +1,19 @@
 #include "charsetdetect.h"
 #include <QString>
+#include <QFile>
 #include <fstream>
 
 QString charsetDetect(QString path) {
+  QFile file(path);
+  if (!file.open(QIODevice::ReadOnly))
+      throw std::invalid_argument("File IO Error");
+
+  QByteArray bytes = file.readAll();
+  const char *contents = bytes.constData();
+  int size = bytes.size();
+
   csd_t csd = csd_open();
-  std::ifstream file(path.toUtf8().constData(),
-                     std::ios::in | std::ios::binary | std::ios::ate);
-  if (file.is_open()) {
-    file.seekg(0, std::ios::end);
-    std::streampos size = file.tellg();
-    char *contents = new char[size];
-    file.seekg(0, std::ios::beg);
-    file.read(contents, size);
-    file.close();
-    csd_consider(csd, contents, size);
-    delete[] contents;
-  } else {
-    throw std::invalid_argument("File IO Error");
-  }
+  csd_consider(csd, contents, size);
+
   return QString(csd_close(csd));
 }
